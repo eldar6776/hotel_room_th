00e4d6cb24ce8bd8e86f0049bb871da0beda20e9
eldar6776~ <34648417+eldar6776@users.noreply.github.com>
2026-06-03 12:27:42 +0200

    Update build number and build date
    Bump VERSION_PATCH from 238 to 241 and update fw/build_no.txt accordingly; update BUILD_DATE in fw/include/version.h to "2026-05-30 17:13:58" to reflect the new build.

--- fw/include/version.h
+++ fw/include/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 238
-#define BUILD_DATE "2026-05-26 19:48:40"
+#define VERSION_PATCH 241
+#define BUILD_DATE "2026-05-30 17:13:58"
 
 #endif
